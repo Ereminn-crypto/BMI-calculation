358501e23f4e34b8668db1519556a2925952146e
Nikita Eremin <ereminn038@gmail.com>
2025-12-06 21:18:57 +0300

    Update: added csv

--- bmi_calculation.py
+++ bmi_calculation.py
@@ -1,12 +1,17 @@
 # determine variables
+import csv
 try:
     height = float(input("Input your height in meters: "))
     weight = float(input("Input your weight: "))
+    category = str()
     # provide computations of BMI
     if weight <= 0 or height <= 0:
         print('Error: weight and height must be positive')
     else:
         BMI = weight / (height ** 2)
+        with open('BMI_history.csv', 'a', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow([height, weight, BMI, category])
         print(f'Your BMI is: {BMI:.2f} kg/m^2')
         if BMI < 18.5:
             print('Underweight')
